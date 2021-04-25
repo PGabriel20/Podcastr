1c97595a9ff7a18b76f63219257514977fa8f9ba
PGabriel20 <bielporreca@gmail.com>
2021-04-24 22:32:16 -0300

    static side generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
+import { useEffect } from "react";
 import { Header } from "../components/Header";
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+//Usando SSR
+//Next irá executar essa função antes de renderizar conteúdo
+//Toda vez que página home é acessada
+//Executado na camada Next (server Node)
+
+/*export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props:{
+      episodes: data,
+    }
+  }
+}*/
+
+//Usando SSG (funciona apenas em produção)
+//Gera uma versão estática da página, apenas
+//mudando o conteudo em um período de tempo
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props:{
+      episodes: data,
+    },
+    revalidate: 60 * 80 *8,
+  }
 }
